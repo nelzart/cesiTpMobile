256ad78080837fbcd227501e9e4d013a229cb56f
SPRAYTED <yanis.sabbari54230@live.fr>
2021-06-21 16:31:20 +0200

    Script de l'API et ajout du répertoire API

---
+++ API/index.php
@@ -0,0 +1,26 @@
+<?php
+
+include("");
+$request = $_SERVER['REQUEST_METHOD'];
+
+function getArticles3Lasts($id){
+
+}
+
+function addArticle(){
+
+}
+
+function updateArticle(){
+
+}
+
+function deleteArticle(){
+
+}
+
+switch($request){
+
+}
+
+?>
