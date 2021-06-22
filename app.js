cbd6847f3052898a634df530c4175066df1910c6
Dorian Bleynie <dorian.bleynie@gmail.com>
2021-06-22 11:55:26 +0200

    ajout de app.js

---
+++ app.js
@@ -0,0 +1,6 @@
+var d = new Date();
+var date = d.getFullYear()+'-'+(d.getMonth()+1)+'-'+d.getDate();
+var hours = d.getHours() + ":" + d.getMinutes() ;
+var fullDate = date+' '+hours;
+console.log(fullDate);
+
