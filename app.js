a47b5ee86102da2389362b891f541d5ba5e27143
Dorian Bleynie <dorian.bleynie@gmail.com>
2021-06-22 12:15:47 +0200

    correction

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
-funtion 
-var d = new Date();
-var date = d.getFullYear()+'-'+(d.getMonth()+1)+'-'+d.getDate();
-var hours = d.getHours() + " : " + d.getMinutes() ;
-var fullDate = date+' '+hours;
+const d = new Date();
+let date = d.getDate() + ' ' + (d.getMonth()+1) + " " + d.getFullYear();
+let hours = d.getHours() + " : " + d.getMinutes() ;
+let fullDate = date+' '+hours;
 console.log(fullDate);
 
 document.getElementsByName('hour').innerHTML += fullDate;
