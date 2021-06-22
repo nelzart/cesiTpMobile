6acd63daa73ccd365da8cb18a30fa7c2f0555a9c
charley04310 <geoffroy-charley@hotmail.fr>
2021-06-22 11:29:51 +0200

    fonction addarticle

---
+++ API/db_connect.php
@@ -0,0 +1,6 @@
+<?php
+	$server = "localhost";
+	$username = "root";
+	$password = "password";
+	$db = "CesiBlog";
+	$conn = mysqli_connect($server, $username, $password, $db);
--- API/index.php
+++ API/index.php
@@ -20,7 +20,40 @@ function getArticles3Lasts($id){
 }
 
 function addArticle(){
+    global $conn;
+                $content = file_get_contents("php://input");
+                $decoded = json_decode($content, true);
 
+                //$relDate = date("Y-m-d H:i:s");  // date gérée par mysql
+
+                $Article_titre = $decoded['Art_Titre'];
+                $Article_Contenu = $decoded['Art_Contenu'];
+                $Article_SousTitre = $decoded['Art_SousTitre'];
+                $Article_Autheur = $decoded['Art_Autheur'];
+                $Categorie_id = $decoded['Cat_Id'];
+               
+             
+
+		$query = mysqli_prepare($conn, "INSERT INTO article(Art_Titre, Art_Contenu, Art_SousTitre, Art_Autheur, Cat_Id) VALUES(?,?,?,?,?)");
+                mysqli_stmt_bind_param($query, "ssssi", $Article_titre, $Article_Contenu, $Article_SousTitre, $Article_Autheur, $Categorie_id);
+
+                if($conn)
+                {
+                        $response1=array(
+                        'status' => 1,
+                        'status_message' =>'Connexion établie et requête envoyée.'
+                        );
+                        mysqli_stmt_execute($query);
+                }
+                else
+                {
+                        $response1=array(
+                        'status' => 0,
+                        'status_message' =>'Erreur connexion et donc la requête non envoyée.'
+                        );
+                };
+                header('Content-Type: application/json');
+                echo json_encode($response1);
 }
 
 function updateArticle($id){
@@ -28,6 +61,7 @@ function updateArticle($id){
 }
 
 function deleteArticle($id){
+
 
 }
 
